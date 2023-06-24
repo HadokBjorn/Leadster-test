de5d08e5a6a24e0fad334f1150c451e3b8ab9798
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-06-23 22:27:10 -0500

    feat: Header Component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import styles from "./Header.module.css" 
 import logo from "public/assets/logo.png"
+import iconTitle from "public/assets/asset-header.png"
 function Header() {
   return (
     <header className={styles.header_container}>
@@ -10,8 +11,13 @@ function Header() {
         <div className={styles.slogan_container}>
             <p className={styles.slogan_container_p1}>WEBNARS EXCLUSIVOS</p>
             <h2>Menos Conversinha,</h2>
-            <h1>Mais Conversão</h1>
-            <p>Conheça as estratégias que mudaramm o jogo e como aplicá-las no seu negócio</p>
+            <h1>
+              Mais Conversão
+              <Image className={styles.slogan_icon_title} src={iconTitle} alt=""/>
+            </h1>
+            <div className={styles.slogan_subtitle_container}>
+              <p className={styles.slogan_subtitle}>Conheça as estratégias que mudaramm o jogo e como aplicá-las no seu negócio</p>
+            </div>
         </div>
     </header>
   )
