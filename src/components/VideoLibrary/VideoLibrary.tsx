a92b169caadb64b47ec7766966d3527032b96492
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-06-27 20:58:24 -0500

    fix: icon play on mouse over

--- src/components/VideoLibrary/VideoLibrary.tsx
+++ src/components/VideoLibrary/VideoLibrary.tsx
@@ -1,13 +1,21 @@
+"use client"
 import Image from "next/image"
 import styles from "./VideoLibrary.module.css"
 import thumbnail from "public/assets/thumbnail.png"
 import {videos} from "../../data/mock.json"
+import {BsFillPlayFill} from "react-icons/bs"
+import { useState } from "react"
 
 interface Props {
     setOpenVideo(value:boolean):void;
     setVideo(value:object):void;
   };
+  type IconVideo ={
+    id: number,
+    selected: boolean
+  }
 function VideoLibrary(props:Props) {
+    const [iconVideo, setIconVideo] = useState<IconVideo>({id: 0, selected: false})
   return (
     <section className={styles.container}>
         <header className={styles.menu_bar_container}>
@@ -26,9 +34,14 @@ function VideoLibrary(props:Props) {
             </div>
         </header>
         <main className={styles.video_bar_container}>
-            {videos.map((video)=>(
-                <article onClick={()=>{props.setOpenVideo(true);props.setVideo({title:video.title, url:video.url, description: video.description})}}>
-                    <Image src={thumbnail} alt="thumbnail"/>
+            {videos.map((video,id)=>(
+                <article onMouseOver={()=>setIconVideo({id:id,selected:true})} key={id+1} onClick={()=>{props.setOpenVideo(true);props.setVideo({title:video.title, url:video.url, description: video.description})}}>
+                    <div>
+                        <Image src={thumbnail} alt="thumbnail"/>
+                        {iconVideo.id === id && iconVideo.selected === true?<span className={styles.play_icon}>
+                            <BsFillPlayFill size={100} color="#FFF"/>
+                        </span>:""}
+                    </div>
                     <h3>{video.title}</h3>
                 </article>
             ))}
